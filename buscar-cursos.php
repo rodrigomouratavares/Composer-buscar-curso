a9b70bfd38c9ea36df42abe04561cad832ca63d9
Rodrigo Moura <rodrigomoura.t@gmail.com>
2021-03-12 02:52:06 -0300

    segundo commit

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,22 +1,28 @@
 <?php
 require 'vendor/autoload.php';
+require 'src/BuscadordeCursos.php';
+use Composers\BuscadordeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
+
 
 $client = new Client([
     'base_uri' => 'https://www.alura.com.br/',
     'verify' => false
 ]);
 
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+
+$buscador = new Buscador($client, $crawler);
+        
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
 
 foreach ($cursos as $curso) {
-   echo $curso->textContent . PHP_EOL;
+   echo $curso . PHP_EOL;
 }
+
+
+
+
---
+++ src/BuscadordeCursos.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Composers\BuscadordeCursos;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador {
+
+    private $httpClient;
+
+    private $crawler;
+
+    public function __construct( ClientInterface $httpClient, crawler $crawler)
+    {
+        //inicializando as propriedades da classe
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url):array
+    {
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+        
+    
+        $this->crawler->addHtmlContent($html);
+        
+        $ElementosCursos = $this->crawler->filter('span.card-curso__nome');
+
+        $cursos=[];
+
+        foreach ($ElementosCursos as $elemento ) {
+         $cursos[] =  $elemento->textContent;
+        }
+
+        return $cursos;
+        
+  
+        
+
+    }
+}
